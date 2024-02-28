8ef9cd4e0ee18f6a0aab39549acb9b47b8d47c8c
tomoro kobori <46224993+kettle-tomorrow@users.noreply.github.com>
2024-02-29 07:54:13 +0900

    write csv

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{error::Error, fs::File, io::Write};
 
 use clap::{Parser, ValueEnum};
 
@@ -20,7 +20,7 @@ pub struct Input {
     entry_types: Publisher,
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 struct Book {
     pub title: String,
     pub author: String,
@@ -34,11 +34,65 @@ pub fn get_args() -> MyResult<Input> {
 }
 
 pub fn run(input: Input) -> MyResult<()> {
+    create_initial_file()?;
     match input.entry_types {
         Publisher::Oreilly => {
-            let books = publisher::oreilly::fetch_books()?;
-            println!("{:#?}", books);
+            let new_books = publisher::oreilly::fetch_books()?;
+            let existed_books = csv_to_books()?;
+            let mut books: Vec<Book> = existed_books.clone();
+            for new_book in new_books {
+                if !existed_books.contains(&new_book) {
+                    books.push(new_book);
+                }
+            }
+            write_books(&books)?;
         }
     }
     Ok(())
+}
+
+fn create_initial_file() -> MyResult<()> {
+    let file = File::open("books.csv");
+    if file.is_ok() {
+        return Ok(());
+    }
+    File::create("books.csv")?;
+    Ok(())
+}
+
+fn write_books(books: &Vec<Book>) -> MyResult<()> {
+    let mut writer = csv::Writer::from_path("books.csv")?;
+    write_header(&mut writer)?;
+    for book in books {
+        write_book(&mut writer, book)?;
+    }
+    writer.flush()?;
+    Ok(())
+}
+
+fn write_header(writer: &mut csv::Writer<File>) -> MyResult<()> {
+    writer.write_record(&["Publisher", "Title", "Author", "Description", "Link", "Date"])?;
+    Ok(())
+}
+
+fn write_book(writer: &mut csv::Writer<File>, book: &Book) -> MyResult<()> {
+    writer.write_record(&["Oreilly", &book.title, &book.author, &book.description, &book.link, &book.release_date])?;
+    Ok(())
+}
+
+fn csv_to_books() -> MyResult<Vec<Book>> {
+    let mut reader = csv::Reader::from_path("books.csv")?;
+    let mut books = vec![];
+    for result in reader.records() {
+        let record = result?;
+        let book = Book {
+            title: record[1].to_string(),
+            author: record[2].to_string(),
+            description: record[3].to_string(),
+            link: record[4].to_string(),
+            release_date: record[5].to_string(),
+        };
+        books.push(book);
+    }
+    Ok(books)
 }
